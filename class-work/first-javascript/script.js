68fbc4aa1614689bc2eb9f0a65e62c7d88e50659
SusanSavariar <sasavariar@gmail.com>
2016-03-23 20:55:07 -0400

    add code to js file

---
+++ class-work/first-javascript/script.js
@@ -0,0 +1,21 @@
+  alert('this is an alert. this is annoying');
+
+  document.write('writing to the document, like a boss');
+
+  document.write(10*10);
+  document.write(100*10);
+  document.write(50-17);
+  document.write(10*10);
+  document.write('Susan Savariar');
+
+var num1 = 10*10;
+var num2 = 100/10
+var num3 = 3000
+
+document.write(num1);
+document.write(num2, num3);
+
+
+  document.getElementById('title').style.color='red';
+
+   document.getElementById('text-chunk').style.background='blue';
