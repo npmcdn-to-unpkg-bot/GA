686ab00ff58ee515f5d62bd52d3eaba2235dece2
SusanSavariar <sasavariar@gmail.com>
2016-04-04 17:51:28 -0400

    finish calculator

--- homework/calculator-box/js/app.js
+++ homework/calculator-box/js/app.js
@@ -22,20 +22,55 @@
 // - .css()
 
 
-$(document).ready(function () {
-var total = 0;
+$(document).ready(function(){
+
+  var total = 0;
 
   $('#a10').click(function(){
     total = total + 10;
     $('#out').html(total);
   });
 
-  $('#a10').click(function(){
+  $('#a20').click(function(){
     total = total + 20;
     $('#out').html(total);
   });
 
-  $('#a10').click(function(){
+  $('#a30').click(function(){
     total = total + 30;
     $('#out').html(total);
   });
+
+  $('#n10').click(function(){
+    total = total - 10;
+    $('#out').html(total);
+  });
+
+  $('#n20').click(function(){
+    total = total - 20;
+    $('#out').html(total);
+  });
+
+  $('#n30').click(function(){
+    total = total - 30;
+    $('#out').html(total);
+  });
+
+  $('#red').click(function(){
+    $('#out').css('background-color', 'red');
+  });
+
+  $('#blue').click(function(){
+    $('#out').css('background-color', 'blue');
+  });
+
+  $('#out').click(function(){
+    // reset total to 0
+    total = 0;
+
+    $('#out')
+      .css('background-color', 'white')
+      .html(total);
+  });
+
+});
