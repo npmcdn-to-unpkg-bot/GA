e594e1ec510e41cc441da784e0000b992291daa6
SusanSavariar <sasavariar@gmail.com>
2016-04-06 20:50:30 -0400

    finish clickcount

--- class-work/click-count/js/app.js
+++ class-work/click-count/js/app.js
@@ -8,5 +8,28 @@
 // 6: Else, change the background-color of <body> to black
 
 $(document).ready(function () {
+  var total = 0;
 
+  $('#clickme').click(function() {
+    // total = total+1;
+    total++;
+    $('#click-num').text(total);
+
+
+        if (total == 5) {
+          $('body').css('background-color','red');
+        }
+        else if (total == 10) {
+          $('body').css('background-color','green');
+        }
+        else if (total == 15) {
+          $('body').css('background-color','blue');
+        }
+        else {
+          $('body').css('background-color','black');
+        }
+
+
+
+  })
 })
