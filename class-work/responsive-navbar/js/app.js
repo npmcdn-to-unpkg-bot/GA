5cd6352890ef617153d11fa17307344da6e2af83
SusanSavariar <sasavariar@gmail.com>
2016-04-20 20:10:04 -0400

    add styling

--- class-work/responsive-navbar/js/app.js
+++ class-work/responsive-navbar/js/app.js
@@ -3,4 +3,8 @@ $(document).ready(function () {
 	$(window).resize(function () {
 		console.log($(window).width())
 	})
-})
+});
+
+$('#burger').click(function () {
+  $('#mobile-nav').slideToggle();
+  });
