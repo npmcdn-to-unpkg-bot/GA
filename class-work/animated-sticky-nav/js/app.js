7671b774a47570960b806d00916d12b10d1094af
SusanSavariar <sasavariar@gmail.com>
2016-04-25 19:21:03 -0400

    make nav bar sticky

--- class-work/animated-sticky-nav/js/app.js
+++ class-work/animated-sticky-nav/js/app.js
@@ -1,2 +1,15 @@
 $(document).ready(function () {
 })
+
+$(window).scroll(function ()) {
+  var scrollTop = $(window).scrollTop()
+
+  if (scrollTop > 20) {
+    $('header').css('background', 'red')
+  }
+  else{
+    $('header').css('background', 'blue')
+  }
+})
+
+})
