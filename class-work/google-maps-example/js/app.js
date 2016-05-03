5c606c6a33605b5814d21693eacb4accc27019c1
SusanSavariar <sasavariar@gmail.com>
2016-05-02 20:56:34 -0400

    add nyc coordinates

--- class-work/google-maps-example/js/app.js
+++ class-work/google-maps-example/js/app.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
+   var map;
+
+   function initMap () {
+    map = new Google.maps.Map(domRef, mapOptions)
+
+   }
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 40.730610, lng: -73.935242},
+          zoom: 12
+        });
+      }
+
+initMap();
+
 })
 
 
