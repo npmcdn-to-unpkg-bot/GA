75249a33cbf13d1e96b4263794bf6f489fb777a8
SusanSavariar <sasavariar@gmail.com>
2016-05-02 22:16:22 -0400

    add inforwindow

--- class-work/google-maps-example/js/app.js
+++ class-work/google-maps-example/js/app.js
@@ -1,27 +1,54 @@
 $(document).ready(function() {
 
-   var map;
+var map;
+var infowindow;
+var search_results = [];
 
-   function initMap () {
-    map = new Google.maps.Map(domRef, mapOptions)
+function initMap() {
+  var pyrmont = {lat: 40.7127 , lng: -74.0059};
 
-   }
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 40.7127, lng: -74.0059},
-          zoom: 12
-        });
-
-      var marker = new google.maps.Marker({
-        position: {lat: 40.7127, lng: -74.0059},
-        map: map,
-        title: 'New York, NY'
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: pyrmont,
+    zoom: 15
   });
 
-      }
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 500,
+    type: ['store']
+  }, callback);
+}
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      console.log(results[i]);
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
+}
+
+
 
 initMap();
 
 })
+
+
 
 
